870e566cfeccf82bd97219da234b0b2e61a228b9
Виталий <vtokarionok@gmail.com>
2023-12-10 20:41:09 +0300

    commit

--- DocTok/ClientApp/src/models/responseModels/IDocumentPage.ts
+++
@@ -1,9 +0,0 @@
-export default interface IDocumentPage{
-    id: number,
-    caption: string,
-    parentId: number | null,
-    content: string,
-    updatedOn: Date,
-    createdOn: Date,
-    createdBy: number,
-}
--- DocTok/Controllers/AuthController.cs
+++
@@ -1,6 +0,0 @@
-﻿namespace DocTok.Controllers
-{
-    public class AuthController
-    {
-    }
-}
